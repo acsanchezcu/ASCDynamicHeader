aea867064b59d07c16a57e5505fc1639f15af3b8
Abel Sánchez Custodio <acsanchezcu@gmail.com>
2016-02-20 12:36:18 +0100

    First commit

---
+++ dynamicHeader/dynamicHeader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dynamicHeader
+//
+//  Created by Abel Sánchez Custodio on 20/2/16.
+//  Copyright © 2016 acsanchezcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
